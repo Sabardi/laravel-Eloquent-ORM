f9ca6544ce43e35dcf957c1e743ec010befe02a5
Sabardi <Sabardibahari@gmail.com>
2024-05-08 17:39:47 +0800

    membuat migrationj dan route dan controller

---
+++ note.md
@@ -0,0 +1,10 @@
+# apa itu orm ?
+ORM adalah singkatan dari "Object-Relational Mapping". Ini adalah teknik yang digunakan untuk memetakan objek dari bahasa pemrograman ke struktur data yang disimpan dalam database relasional.
+
+database terdiri dari kumpulan tabel yang saling
+terhubung. Di dalam setiap tabel, data disimpan dalam bentuk baris dan kolom. ORM dipakai
+untuk mengubah baris dan kolom ini menjadi sebuah object. Nantinya, setiap kolom akan
+menjadi property dari object tersebut dan cara  melakukan inputan nya pun sedikit berbeda
+
+
+bisa menggunakan database sebelum nya
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/cekObject', [MahasiswaController::class,'cekObject']);
+Route::get('/insert', [MahasiswaController::class,'insert']);
+Route::get('/massAssignment', [MahasiswaController::class,'massAssignment']);
+ Route::get('/massAssignment2',[MahasiswaController::class,'massAssignment2']);
+
+Route::get('/update', [MahasiswaController::class,'update']);
+Route::get('/updateWhere', [MahasiswaController::class,'updateWhere']);
+Route::get('/massUpdate', [MahasiswaController::class,'massUpdate']);
+Route::get('/delete', [MahasiswaController::class,'delete']);
+Route::get('/destroy', [MahasiswaController::class,'destroy']);
+Route::get('/massDelete', [MahasiswaController::class,'massDelete']);
+Route::get('/all', [MahasiswaController::class,'all']);
+Route::get('/all-view', [MahasiswaController::class,'allView']);
+Route::get('/get-where', [MahasiswaController::class,'getWhere']);
+Route::get('/testWhere', [MahasiswaController::class,'testWhere']);
+Route::get('/first', [MahasiswaController::class,'first']);
+Route::get('/find', [MahasiswaController::class,'find']);
+Route::get('/latest', [MahasiswaController::class,'latest']);
+Route::get('/limit', [MahasiswaController::class,'limit']);
+Route::get('/skipTake', [MahasiswaController::class,'skipTake']);
+Route::get('/softDelete', [MahasiswaController::class,'softDelete']);
+Route::get('/withTrashed', [MahasiswaController::class,'withTrashed']);
+Route::get('/restore', [MahasiswaController::class,'restore']);
+Route::get('/forceDelete', [MahasiswaController::class,'forceDelete']);
